c0716e214cfdc7cfae2aa66e19883a42433bbd86
LEE IN HAENG <lih0420@naver.com>
2024-05-05 15:03:33 +0900

    aspnet-core-rust-wasm helloworld

---
+++ HelloRustWasm/HelloRustWasm/Controllers/HelloWasmController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloRustWasm.Controllers
+{
+    public class HelloWasmController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
